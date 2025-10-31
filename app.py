34e27543325892c0e9e865cb78e4863840ef2a27
frnietz <57188938+frnietz@users.noreply.github.com>
2025-10-31 13:36:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ else:
             y=y,
             zmin=-1,
             zmax=1,
-            colorscale="thermal",          # vivid contrasting colors
+            colorscale="darkmint",          # vivid contrasting colors
             colorbar=dict(title="ρ"),
             hovertemplate='%{x} vs %{y}: <b>%{z:.2f}</b><extra></extra>',
         ))
